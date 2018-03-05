84f73fd193bc25e41e8cfad9c8cb60ef35cd57e5
Ollie Norris <ollie.norris@adylic.com>
2018-03-05 07:37:51 +0000

    minor changes

--- app.js
+++ app.js
@@ -6,28 +6,39 @@ app.functions = {}
 
 app.dom.body = document.querySelector("body");
 
+var numberOfSlides;
+
 // init function starts on page load
 app.functions.init = function(slideData) {
 
-    console.log("App is running.");
+    console.log("init is running.");
 
     // create containers for slider
     var createContainers = app.functions.createContainers();
 
     // get number of slides
-    var numberOfSlides = slideData.length;
+    	numberOfSlides = slideData.length;
 
     // create slide elements
-    var slideElements = app.functions.createElements(numberOfSlides);
+    var slideElements = app.functions.createSlides(slideData);
+
+    var slideContainer = document.querySelector(".slide-container");
 
     // append slides to container
-    	app.functions.appendChild(slideElements)
+    app.functions.appendElements(slideElements, slideContainer);
 
-    console.log(slideElements)
+    // create slide elements
+    var buttonElements = app.functions.createButtons(numberOfSlides);
 
+    var buttonContainer = document.querySelector(".slide-button-container");
+
+    // append slides to container
+    app.functions.appendElements(buttonElements, buttonContainer);
 }
 
 app.functions.slideData = function() {
+
+	console.log("Retrieving slide data.");
 
     var getSlideData = new XMLHttpRequest();
 
@@ -54,9 +65,6 @@ app.functions.slideData = function() {
 
     getSlideData.send();
 
-
-    
-
 }
 
 app.functions.createContainers = function() {
@@ -70,15 +78,26 @@ app.functions.createContainers = function() {
     // append element
     app.dom.body.appendChild(pageContainer);
 
+    // create slides container
+    var slidesContainer = document.createElement("div");
+    slidesContainer.classList.add("slide-container");
+
+    // append element
+    pageContainer.appendChild(slidesContainer);
+
     // create arrows
     var arrowLeft = document.createElement("div");
     arrowLeft.classList.add("arrow", "arrow-left");
+    arrowLeft.setAttribute("data-arrowdirection", "left");
+    arrowLeft.addEventListener("click", app.functions.arrowButtonClick);
 
     // append element to container
     pageContainer.appendChild(arrowLeft);
 
     var arrowRight = document.createElement("div");
     arrowRight.classList.add("arrow", "arrow-right")
+    arrowRight.setAttribute("data-arrowdirection", "right");
+    arrowRight.addEventListener("click", app.functions.arrowButtonClick);
 
     // append element to container
     pageContainer.appendChild(arrowRight);
@@ -107,20 +126,106 @@ app.functions.createElements = function(numberOfElements) {
 
 }
 
-app.functions.appendElements = function(nameOfElement) {
+app.functions.createSlides = function(slideData) {
 
-	var container = document.querySelector(".container");
+	console.log("Creating slides.");
 
-	for(var i = 0; i < nameOfElement.length; i++) {
+    var numberOfSlides = slideData.length
 
-		container.appendChild(nameOfElement)
+    var slideElements = app.functions.createElements(numberOfSlides);
+
+    var slideCounter = 1;
+    var slideDatasetCounter = 1;
+
+    for (var i = 0; i < slideData.length; i++) {
+
+        slideElements[i].classList.add("slide", "slide-" + slideCounter++);
+        slideElements[i].setAttribute("data-slidenumber", slideDatasetCounter++);
+
+        var slideTitle = slideData[i].slide_title;
+        var slideMessage = slideData[i].slide_message;
+        var slideBgColour = slideData[i].slide_background;
+
+        slideElements[i].style.backgroundColor = slideBgColour;
+
+    }
+
+    return slideElements
+}
+
+app.functions.createButtons = function(numberOfSlides) {
+
+	console.log("Creating slide buttons.")
+    var buttonElements = app.functions.createElements(numberOfSlides);
+
+    var buttonCounter = 1;
+    var buttonTextCounter = 1;
+
+    for (var i = 0; i < numberOfSlides; i++) {
+
+        buttonElements[i].classList.add("button", "button-" + buttonCounter++);
+
+        buttonElements[i].addEventListener("click", app.functions.slideButtonClick)
+
+    }
+
+    buttonElements[0].classList.add("active");
+
+    return buttonElements
+}
+
+var currentSlide = 1;
+
+app.functions.slideButtonClick = function(e) {
+
+	var buttonTarget = e.target;
+	var buttonTargetSlideNumber = e.target.dataset.slidenumber;
+
+	var buttons = document.querySelectorAll(".button");
+
+	for(var i = 0; i < buttons.length; i++) {
+
+		buttons[i].classList.remove("active");
 
 	}
 
+		buttonTarget.classList.add("active")
 
+}
 
+app.functions.arrowButtonClick = function(e) {
 
+	var arrowTarget = e.target;
+	var arrowTargetDirection = e.target.dataset.arrowdirection;
 
+	var slideContainer = document.querySelector(".slide-container");
+
+	if(arrowTargetDirection === "left") {
+		currentSlide--
+	}
+
+	if(arrowTargetDirection === "right") {
+		currentSlide++
+	}
+
+	if(currentSlide <1) {
+		currentSlide = 1;
+	}
+
+	if(currentSlide>numberOfSlides) {
+		currentSlide = numberOfSlides
+	}
+}
+
+app.functions.appendElements = function(nameOfElement, appendToElement) {
+
+    var container = document.querySelector(".container");
+
+    for (var i = 0; i < nameOfElement.length; i++) {
+
+        appendToElement.appendChild(nameOfElement[i])
+
+    }
 }
 
 
